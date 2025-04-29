37fc215688f60338012b57240943f7da2a6d5fde
omitul.islam <omitul.islam@brainstation-23.com>
2025-04-29 17:29:56 +0600

    removed reload after logout

--- src/App.jsx
+++ src/App.jsx
@@ -80,7 +80,6 @@ function App() {
               className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
               onClick={() => {
                 localStorage.removeItem("token");
-                window.location.reload();
                 navigate('/');
               }}
             >
